0f1df7bdbd127bee1c2d7cad9686273bcebc2815
tunim73 <antonio.netto973@gmail.com>
2024-03-20 10:17:14 -0300

    update: reorganização do config

--- src/config.php
+++ src/config.php
@@ -6,37 +6,37 @@ $dynamicDirectory = '/../imgLondon';
 $fileName = 'output.mp4';
 
 
-
-
-/* Configurações do vídeo */
-
-$imageTime = 2;
-$quality = 'hd720';
-$outputFrameRate = 25;
-$zoomDuration = $outputFrameRate * $imageTime;
-
-$transitionTime = 0.5;
-
-
-
-
-$codec = "libx264";
-$format = "yuv420p";
-
-$finalConfig = "-pix_fmt $format -c:v $codec \-map \"[out]\" $fileName -y";
-
-
-
 /* Constantes */
 
 //busca de arquivos
 $imageDirectory = dirname(__FILE__) . $dynamicDirectory;
 $files = glob($imageDirectory . '/*.jpg');
 
+$audioDirectory = dirname(__FILE__) . "/../audio/audio1.mp3";
 
 // comandos estruturais do ffmpeg
 $cmd = "ffmpeg \\";
 $filter_complex = "-filter_complex \"";
+// configurações 
+$codec = "libx264";
+$format = "yuv420p";
+$quality = 'hd720';
 
 
 
+/* Configurações dinâmicas */
+
+$imageTime = 2; //tempo escolhido para cada imagem
+
+$videoTime = $imageTime * count($files) + 1;
+
+$durationAfade = $imageTime - 1;
+$stAfade = $videoTime - $durationAfade - 1;
+
+$outputFrameRate = 25;
+$zoomDuration = $outputFrameRate * $imageTime;
+
+$transitionTime = 0.5;
+
+/* não retirar, isso corrige a questão do tempo de cada imagem, visto que elas são limitadas pelo offset em um segundo a menos */
+$imageTime++;
